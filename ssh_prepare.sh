ef4e29142d39c7285d26dc7e724e3eead764ac5a
Jan Abrell <abrell@posteo.de>
2025-01-25 11:51:59 +0100

    update action

--- ssh_prepare.sh
+++
@@ -1,9 +0,0 @@
-apt-get update -y
-apt-get install -y openssh-client
-mkdir -p ~/.ssh
-echo "$SSH_PRIVATEKEY" > ~/.ssh/id_ed25519
-chmod 0600 ~/.ssh/id_ed25519
-echo "Host *\n\tStrictHostKeyChecking accept-new\n\n" > ~/.ssh/config
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_ed25519
-echo $SSH_EC2 > ~/.ssh/toypage.pem
