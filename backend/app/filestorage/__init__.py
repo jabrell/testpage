aa9e0d76cb053cc8a7baf2d1c0dc4ffd86ed004f
Jan <abrell@posteo.de>
2025-03-19 18:09:18 +0100

    s3 storage backend

--- backend/app/filestorage/__init__.py
+++ backend/app/filestorage/__init__.py
@@ -1,4 +1,5 @@
 from .local_storage import LocalFileStorage
 from .protocol import FileStorageProtocol
+from .s3_storage import S3FileStorage
 
-__all__ = ["LocalFileStorage", "FileStorageProtocol"]
+__all__ = ["LocalFileStorage", "FileStorageProtocol", "S3FileStorage"]
