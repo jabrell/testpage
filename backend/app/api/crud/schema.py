1aab7fcefc030390f580f07ee657d05f674a9742
Jan Abrell <abrell@posteo.de>
2025-03-24 11:57:34 +0100

    transfer

--- backend/app/api/crud/schema.py
+++ backend/app/api/crud/schema.py
@@ -1,5 +1,4 @@
-from sqlalchemy.exc import SQLAlchemyError
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from app.models.schema import RawJsonSchema
 from app.schema_manager import SchemaManager
@@ -18,7 +17,38 @@ def create_schema(
         db.add(db_schema)
         db.commit()
         db.refresh(db_schema)
-    except SQLAlchemyError as e:
+    except Exception as e:
         db.rollback()
         raise ValueError("Could not insert schema to db") from e
     return db_schema
+
+
+def read_schema(
+    db: Session, schema_id: int | None = None, schema_name: str | None = None
+) -> RawJsonSchema:
+    """Read a schema from the database
+
+    Args:
+        db (Session): Database session
+        schema_id (int, optional): Schema id. Defaults to None.
+        schema_name (str, optional): Schema name. Defaults to None.
+
+    Returns:
+        RawJsonSchema: Schema object
+
+    Raises:
+        ValueError: If schema is not found
+    """
+    if schema_id is None and schema_name is None:
+        raise ValueError("Either schema_id or schema_name must be provided")
+    if schema_id is not None and schema_name is not None:
+        raise ValueError("Only one of schema_id or schema_name must be provided")
+    if schema_id is not None:
+        schema = db.get(RawJsonSchema, schema_id)
+    elif schema_name is not None:
+        schema = db.exec(
+            select(RawJsonSchema).filter(RawJsonSchema.name == schema_name).first()
+        )
+    if schema is None:
+        raise ValueError("Schema not found")
+    return schema
--- backend/tests/schema_manager/test_crud.py
+++ backend/tests/schema_manager/test_crud.py
@@ -1,31 +1,42 @@
 import pytest
-import yaml
 from sqlalchemy.orm import Session
 
-from app.api.crud.schema import create_schema
+from app.api.crud.schema import create_schema, read_schema
 from app.schema_manager import SchemaManager
 
 from .settings import sweet_valid
 
 
-def test_create_schema(db: Session) -> None:
-    data = str(sweet_valid).encode("utf-8")
-    content_str = data.decode("utf-8")
-    t = yaml.safe_load(content_str)
-    print(t)
-    schema_manger = SchemaManager()
-    schema = create_schema(db=db, data=data, schema_manager=schema_manger)
+@pytest.fixture(scope="module")
+def data() -> bytes:
+    return str(sweet_valid).encode("utf-8")
+
+
+@pytest.fixture(scope="module")
+def schema_manager() -> SchemaManager:
+    return SchemaManager()
+
+
+def test_create_schema(db: Session, data: bytes, schema_manager: SchemaManager) -> None:
+    schema = create_schema(db=db, data=data, schema_manager=schema_manager)
     assert schema.name == "test"
     assert schema.description == "test"
     assert schema.jsonschema == sweet_valid
     assert not schema.is_active
 
 
-def test_create_schema_raise(db: Session) -> None:
-    data = str(sweet_valid).encode("utf-8")
-    content_str = data.decode("utf-8")
-    t = yaml.safe_load(content_str)
-    print(t)
-    schema_manger = SchemaManager()
+def test_create_schema_raise(
+    db: Session, data: bytes, schema_manager: SchemaManager
+) -> None:
+    create_schema(db=db, data=data, schema_manager=schema_manager)
     with pytest.raises(ValueError):
-        create_schema(db=db, data=data, schema_manager=schema_manger)
+        create_schema(db=db, data=data, schema_manager=schema_manager)
+
+
+@pytest.mark.skip
+def test_read_schema_by_id(
+    data: bytes, db: Session, schema_manager: SchemaManager
+) -> None:
+    inserted = create_schema(db=db, data=data, schema_manager=schema_manager)
+    schema = read_schema(db=db, schema_id=inserted.id)
+    assert schema == inserted
