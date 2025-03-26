0952047c31d0ff4a8579d5a5d613ca4e411cf260
Jan <abrell@posteo.de>
2025-03-26 19:41:31 +0100

    restored path to webui

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,22 @@
-# simple hello world with fast api
-
+from a2wsgi import WSGIMiddleware
 from fastapi import FastAPI
 
 from app.api.main import api_router
 from app.core.config import settings
+from app.middleware.request_logging import RequestLoggingMiddleware
+from app.webui import app as flask_app
 
 app = FastAPI()
 
+
 # include routes
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+# middleware
+app.add_middleware(RequestLoggingMiddleware)
+
+# mount the flask app
+app.mount("/webui/", WSGIMiddleware(flask_app))  # types: ignore
 
 
 @app.get("/")
