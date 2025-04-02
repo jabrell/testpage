aa3bd8bcd46de86f15ecc8b3eace3763e3e96a47
Jan <abrell@posteo.de>
2025-04-02 11:08:50 +0200

    updated docker compose

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -30,7 +30,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
 
-    # Postges settings
+    # Postgres settings
     POSTGRES_SERVER: str
     POSTGRES_PORT: int
     POSTGRES_USER: str
